4f3b12fc78b88747948a228cda3dc349e6ea12b5
Amianate <alexwild737@gmail.com>
2023-11-24 01:37:01 -0500

    Started integrating Assignment 2 and 1

--- 400003165/framework/ORM/orm.php
+++ 400003165/framework/ORM/orm.php
@@ -32,10 +32,21 @@ class orm{
     public function addUser($name, $email, $pw, $role = "Researcher"){
         $pw = password_hash($pw, PASSWORD_DEFAULT);
 
-        $query = $this->conn->prepare("INSERT INTO `users`(`id`, `username`, `password`, `email`, `role`) VALUES ('NULL','" . $name . "' ,'" . $pw . "','" . $email . "','" . $role . "')");
+        $query = $this->conn->prepare("INSERT INTO `users`(`id`, `username`, `password`, `email`, `role`) VALUES (NULL, :name, :pw, :email, :role)");
+            // Using bindParam to reduce secutiry risk (SQL Injection)
+        $query->bindParam(':name', $name);
+        $query->bindParam(':pw', $pw);
+        $query->bindParam(':email', $email);
+        $query->bindParam(':role', $role);
+
         $query->execute();
         
-        $idQuery = $this->conn->prepare("SELECT id FROM `users` WHERE username = '" . $name . "' ");
+        // $query = $this->conn->prepare("INSERT INTO `users`(`id`, `username`, `password`, `email`, `role`) VALUES ('NULL','" . $name . "' ,'" . $pw . "','" . $email . "','" . $role . "')");
+        // $query->execute();
+        
+        $idQuery = $this->conn->prepare("SELECT id FROM `users` WHERE username = ':idName' ");
+        $query->bindParam(':idName', $name);
+
         $idQuery->execute();
 
         $result = $idQuery->fetch(PDO::FETCH_ASSOC);
